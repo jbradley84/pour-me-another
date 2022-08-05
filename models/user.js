52b67fceca77c333bbbaba4e3e38d38dc78be9a8
tlor0026 <tlor0026@gmail.com>
2022-08-04 19:22:19 -0500

    updates

--- models/user.js
+++ models/user.js
@@ -3,10 +3,10 @@ const { Model, DataTypes } = require('sequelize');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
 
-// Initialize Product model (table) by extending off Sequelize's Model class
+// Initialize user model (table) by extending off Sequelize's Model class
 class user extends Model { }
 
-// set up fields and rules for Product model
+// set up fields and rules for user model
 user.init(
   {
     // define columns
