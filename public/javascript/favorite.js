21c38922189f269e6af1c79491f5e777131688ba
jbradley84 <utbradley84@yahoo.com>
2022-08-06 13:53:37 -0500

    added favorite.js to public/javascript files to handle beverage favoriting

---
+++ public/javascript/favorite.js
@@ -0,0 +1,24 @@
+async function favoriteClickHandler(event) {
+   event.preventDefault();
+
+   const id = window.location.toString().split('/')[
+      window.location.toString().split('/').length - 1
+   ];
+   console.log(id);
+   const response = await fetch('/api/beverage/favorite', {
+      method: 'PUT',
+      body: JSON.stringify({
+         beverage_id: id
+      }),
+      headers: {
+         'Content-Type': 'application/json'
+      }
+   });
+   if (response.ok) {
+      document.location.reload();
+   } else {
+      alert(response.statusText);
+   }
+}
+
+document.querySelector('.favorite-btn').addEventListener('click', favoriteClickHandler);
