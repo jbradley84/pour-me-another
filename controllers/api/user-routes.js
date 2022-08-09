4436d9c6e5baef90a78f58e7cd260ca4a52988c9
jbradley84 <utbradley84@yahoo.com>
2022-08-08 20:17:20 -0500

    updated mybar-routes

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -3,102 +3,105 @@ const { User, Beverage, Favorite } = require('../../models');
 
 // get all users
 router.get('/', (req, res) => {
-  User.findAll({
-    attributes: { exclude: ['password'] }
-  })
-    .then(dbUserData => res.json(dbUserData))
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
+   User.findAll({
+      attributes: { exclude: ['password'] }
+   })
+      .then(dbUserData => res.json(dbUserData))
+      .catch(err => {
+         console.log(err);
+         res.status(500).json(err);
+      });
 });
 
 router.get('/:id', (req, res) => {
-  User.findOne({
-    attributes: { exclude: ['password'] },
-    where: {
-      id: req.params.id
-    },
-    include: [
-      {
-        model: Beverage,
-        attributes: ['beverage_name'],
-        through: Favorite,
-        as: 'favorite_beverage'
-      }
-    ]
-  })
-    .then(dbUserData => {
-      if (!dbUserData) {
-        res.status(404).json({ message: 'No user found with this id' });
-        return;
-      }
-      res.json(dbUserData);
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
+   User.findOne({
+      attributes: { exclude: ['password'] },
+      where: {
+         id: req.params.id
+      },
+      include: [
+         {
+            model: Beverage,
+            attributes: ['beverage_name'],
+            through: Favorite,
+            as: 'favorite_beverage'
+         }
+      ]
+   })
+      .then(dbUserData => {
+         if (!dbUserData) {
+            res.status(404).json({ message: 'No user found with this id' });
+            return;
+         }
+         res.json(dbUserData);
+      })
+      .catch(err => {
+         console.log(err);
+         res.status(500).json(err);
+      });
 });
 
 router.post('/', (req, res) => {
-  User.create({
-    username: req.body.username,
-    email: req.body.email,
-    password: req.body.password
-  })
-    .then(dbUserData => {
-      req.session.save(() => {
-        req.session.user_id = dbUserData.id;
-        req.session.username = dbUserData.username;
-        req.session.loggedIn = true;
-  
-        res.json(dbUserData);
+   User.create({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password
+   })
+      .then(dbUserData => {
+         req.session.save(() => {
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+
+            res.json(dbUserData);
+         });
+      })
+      .catch(err => {
+         console.log(err);
+         res.status(500).json(err);
       });
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
 });
 
 router.post('/login', (req, res) => {
-  User.findOne({
-    where: {
-      email: req.body.email
-    }
-  }).then(dbUserData => {
-    if (!dbUserData) {
-      res.status(400).json({ message: 'No user with that email address!' });
-      return;
-    }
+   User.findOne({
+      where: {
+         email: req.body.email
+      }
+   }).then(dbUserData => {
+      if (!dbUserData) {
+         res.status(400).json({ message: 'No user with that email address!' });
+         return;
+      }
 
-    const validPassword = dbUserData.checkPassword(req.body.password);
+      const validPassword = dbUserData.checkPassword(req.body.password);
 
-    if (!validPassword) {
-      res.status(400).json({ message: 'Incorrect password!' });
-      return;
-    }
+      if (!validPassword) {
+         res.status(400).json({ message: 'Incorrect password!' });
+         return;
+      }
 
-    req.session.save(() => {
-      req.session.user_id = dbUserData.id;
-      req.session.username = dbUserData.username;
-      req.session.loggedIn = true;
-  
-      res.json({ user: dbUserData, message: 'You are now logged in!' });
-    });
-  });
+      req.session.save(() => {
+         req.session.user_id = dbUserData.id;
+         req.session.username = dbUserData.username;
+         req.session.loggedIn = true;
+
+         res.json({ user: dbUserData, message: 'You are now logged in!' });
+      });
+   })
+      .catch(err => {
+         res.status(400).json({ message: err.message })
+      })
 });
 
 router.post('/logout', (req, res) => {
-  if (req.session.loggedIn) {
-    req.session.destroy(() => {
-      res.status(204).end();
-    });
-  }
-  else {
-    res.status(404).end();
-  }
+   if (req.session.loggedIn) {
+      req.session.destroy(() => {
+         res.status(204).end();
+      });
+   }
+   else {
+      res.status(404).end();
+   }
 });
 
 module.exports = router;
--- controllers/mybar-routes.js
+++ controllers/mybar-routes.js
@@ -3,34 +3,31 @@ const sequelize = require('../config/connection');
 const { Beverage, User, Favorite } = require('../models');
 const withAuth = require('../utils/auth');
 
-// get session user data including favorited beverages for My Bar list
+// get list of all beverages favorited by session user
 router.get('/', withAuth, (req, res) => {
-  console.log(req.session);
-  User.findOne({
-   attributes: { exclude: ['password'] },
-   // where: {
-   //   id: req.params.id
-   // },
-   include: [
-     {
-       model: Beverage,
-       attributes: ['beverage_name'],
-       through: Favorite,
-       as: 'favorite_beverage'
-     }
-   ]
-})
-.then(dbUserData => {
-   console.log(dbUserData);
-  const beverages = dbUserData.map(beverage => beverage.get({ plain: true }));
-  console.log(beverages);
-  res.render('mybar', { beverages, loggedIn: true });
-})
-.catch(err => {
-  console.log(err);
-  res.status(500).json(err);
-});
+   Beverage.findAll({
+      where: {
+         // favorite.user_id = session user id
+      },
+      attributes: [
+         'id',
+         'beverage_name',
+         'beverage_type',
+         [sequelize.literal('(SELECT COUNT(*) FROM favorite WHERE beverage.id = favorite.beverage_id)'), 'favorite_count']
+      ]
+   })
+      .then(dbBeverageData => {
+         console.log(dbBeverageData);
+         const beverages = dbBeverageData.map(beverage => beverage.get({ plain: true }));
+         console.log(beverages);
+         res.render('mybar', { beverages, loggedIn: true });
+      })
+      .catch(err => {
+         console.log(err);
+         res.status(500).json(err);
+      });
 });
 
 module.exports = router;
+
 
