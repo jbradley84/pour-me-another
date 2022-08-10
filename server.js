0be2a8daefe35002582618ca1e3049164e6e725f
jbradley84 <utbradley84@yahoo.com>
2022-08-10 18:58:45 -0500

    connecting to heroku

--- server.js
+++ server.js
@@ -26,12 +26,12 @@ const helpers = require('./utils/helpers');
 
 const hbs = exphbs.create({ helpers });
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(require('./controllers'));
 
