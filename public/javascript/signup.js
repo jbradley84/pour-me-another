3cf237416300bbe1254e607de68f154f99c08295
jbradley84 <utbradley84@yahoo.com>
2022-08-10 18:53:35 -0500

    adding console log to signup.js

--- public/javascript/signup.js
+++ public/javascript/signup.js
@@ -6,6 +6,7 @@ async function signupFormHandler(event) {
    const password = document.querySelector('#password-signup').value.trim();
 
    if (username && email && password) {
+      console.log('farewell rose');
       const response = await fetch('/api/users', {
          method: 'POST',
          body: JSON.stringify({
