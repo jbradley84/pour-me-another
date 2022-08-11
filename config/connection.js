6b22b1ca341fac11b6fdb62d213afb33bc572fd9
jbradley84 <utbradley84@yahoo.com>
2022-08-10 19:11:35 -0500

    troubleshooting heroku

--- config/connection.js
+++ config/connection.js
@@ -4,11 +4,11 @@ require('dotenv').config();
 
 // create connection to our db
 const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-      host: 'localhost',
+   ? new Sequelize(process.env.JAWSDB_URL)
+   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+      host: process.env.DB_HOST || 'localhost',
       dialect: 'mysql',
       port: 3306
-    });
+   });
 
 module.exports = sequelize;
